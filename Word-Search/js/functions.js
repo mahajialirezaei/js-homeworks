93781fb60ff83d22d119b3949681a9829f6e5bc0
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-07-05 18:48:07 +0330

    update files

--- Word-Search/js/functions.js
+++ Word-Search/js/functions.js
@@ -1,8 +1,17 @@
+var originaltxt = "";
+
 function pageLoad(){
-    originaltxt = document.getElementById('textWholePage').innerHTML
+    originaltxt = document.getElementById('textWholePage').innerHTML;
 }
+
 function findWord(){
-    let phrase = document.getElementById('textInput').value
-    let txt = originaltxt
-    document.getElementById('text').innerHTML = txt.split(phrase).join("<span class='highlight'>"+phrase+"</span>")
+    let phrase = document.getElementById('textInput').value;
+    let txt = originaltxt;
+
+    if (phrase) {
+        let highlightedText = txt.split(phrase).join("<span class='highlight'>"+phrase+"</span>");
+        document.getElementById('text').innerHTML = highlightedText;
+    } else {
+        document.getElementById('text').innerHTML = txt;
+    }
 }
